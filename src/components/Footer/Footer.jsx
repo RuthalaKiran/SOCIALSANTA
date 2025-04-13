faad8a7cded1be7f5d67573a8912ccf85e446447
Ruthala kiran <kiranruthalakiran@gmail.com>
2025-04-13 15:57:31 +0530

    contact added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
   return (
     <footer className="w-full py-10 px-5 sm:mt-10 sm:py-10 sm:px-[100px] flex flex-col sm:flex-row gap-10 items-center justify-between bg-[#412675]">
       <div className="left text-white flex items-start flex-col">
-        <p>socialsanta2@gmail.com</p>
-        <p>+91 9137559755</p>
+        <p>helpsocialsanta@gmail.com</p>
+        <p>+91 879275739</p>
       </div>
       <div className="right flex gap-5 text-[25px] text-gray-300">
        <a href="https://www.instagram.com/agencysocialsanta/" target="_blank">  <FaInstagram className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" /></a>

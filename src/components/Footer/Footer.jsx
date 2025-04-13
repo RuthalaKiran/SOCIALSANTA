87900cf0e27ae14b8c6ab254056c2b5e1f12614a
Ruthala kiran <kiranruthalakiran@gmail.com>
2025-04-13 16:01:36 +0530

    added address

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,16 +8,38 @@ import { FaFacebook } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="w-full py-10 px-5 sm:mt-10 sm:py-10 sm:px-[100px] flex flex-col sm:flex-row gap-10 items-center justify-between bg-[#412675]">
-      <div className="left text-white flex items-start flex-col">
+      <div className="left text-white flex items-start gap-1 flex-col">
+        <p>
+          Pramod bunglow, 4th cross, SV colony, congress rd, near panchamrut
+          hotel, tilakwadi Belagavi 590006
+        </p>
         <p>helpsocialsanta@gmail.com</p>
         <p>+91 879275739</p>
       </div>
       <div className="right flex gap-5 text-[25px] text-gray-300">
-       <a href="https://www.instagram.com/agencysocialsanta/" target="_blank">  <FaInstagram className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" /></a>
-      <a href="https://www.linkedin.com/company/social-santa-syc/posts/?feedView=all" target="_blank">  <FaLinkedinIn className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" /></a>
-      <a href="https://www.pinterest.com/agencysocialsanta/" target="_blank">  <FaPinterest className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" /></a>
-       <a href=""> <AiOutlineYoutube className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" /></a>
-       <a href=""> <FaFacebook className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" /></a>
+        <a href="https://www.instagram.com/agencysocialsanta/" target="_blank">
+          {" "}
+          <FaInstagram className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/social-santa-syc/posts/?feedView=all"
+          target="_blank"
+        >
+          {" "}
+          <FaLinkedinIn className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" />
+        </a>
+        <a href="https://www.pinterest.com/agencysocialsanta/" target="_blank">
+          {" "}
+          <FaPinterest className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" />
+        </a>
+        <a href="">
+          {" "}
+          <AiOutlineYoutube className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" />
+        </a>
+        <a href="">
+          {" "}
+          <FaFacebook className="cursor-pointer hover:scale-110 hover:text-gray-100 duration-200" />
+        </a>
       </div>
     </footer>
   );
